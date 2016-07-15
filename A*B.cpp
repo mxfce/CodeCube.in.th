aa601e03fffe72361b3d684256c5e19353a1bf9d
mxfce <mxfcemxfce@gmail.com>
2016-07-15 15:50:13 +0700

    เฉลยข้อ A*B

---
+++ A*B.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <cstring> 
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+typedef long long ll;
+int main()
+{
+	ll a,b;
+	scanf("%lld %lld",&a,&b);
+	printf("%lld\n",a * b);
+	return 0;
+}
